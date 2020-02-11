6ee406534811c08b9fd36a2263b99cb485ede07d
Mike Zurzolo <mikezurzolo@gmail.com>
2020-02-11 17:20:56 -0500

    candidate4, mysql database

--- build.sh
+++ build.sh
@@ -5,10 +5,19 @@ if [ "$0" = "$BASH_SOURCE" ]; then
     exit 1
 fi
 
+
+# build the django image
+pushd src/docker/db-docker
+sudo docker build -t mysqldb .
+popd
+
 # build the django image
 pushd src/docker/django-docker
 sudo docker build -t djangotrial .
 popd
 
 printf "\nTo run the django image:\n"
-printf "    sudo docker run -p8000:8000 -it djangotrial\n"
+printf "    sudo docker run -p8000:8000 -d djangotrial\n"
+
+printf "\nTo run the database image:\n"
+printf "    sudo docker run -p 3306:3306 -d mysqldb \n"
