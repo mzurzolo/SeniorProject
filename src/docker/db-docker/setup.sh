8bf7c8df068a05844becb40ecb51e89a8defbf00
Mike Zurzolo <mikezurzolo@gmail.com>
2020-02-12 07:00:03 -0500

    database and user init done with sql script, dropped into /docker-entrypoint-initdb.d/

--- src/docker/db-docker/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-user=root
-password=password
-newuser=default
-newpass=password
-sleep 10s
-echo password > /tmp/password.txt
-mysql -u"$user" -p"$(cat /tmp/password.txt)" -e"CREATE USER '$newuser'@'localhost' IDENTIFIED BY '$newpass'; GRANT ALL PRIVILEGES ON *.* TO '$newuser'@'localhost' WITH GRANT OPTION; GRANT ALL PRIVILEGES ON *.* TO '$newuser'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-rm /tmp/password.txt
