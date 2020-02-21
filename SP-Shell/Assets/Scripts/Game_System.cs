18e12c14cbf9f9e8522dabaff14115c044dffa60
Syndicist <31106886+Syndicist@users.noreply.github.com>
2020-02-21 10:49:08 -0500

    Initial Project Shell
    When run, this project picks one of two players as the winner randomly, and outputs that result to a text file.

---
+++ SP-Shell/Assets/Scripts/Game_System.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Game_System : MonoBehaviour
+{
+    public GameObject player_container;
+    public GameObject Text;
+    private Player[] players;
+    private Random rnd = new Random();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = player_container.GetComponentsInChildren<Player>();
+
+        Text.GetComponent<UnityEngine.UI.Text>().text += " = " + players[Random.Range(0, 2)].name;
+
+        //create Folder
+
+
+        string m_Path = Application.dataPath;
+
+        //Output the Game data path to the console
+        Debug.Log("dataPath : " + m_Path);
+        File.WriteAllText(m_Path + "/winner.txt", "Winner is " + players[Random.Range(0, 2)].name);
+    }
+}
