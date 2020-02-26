938421667f37489d7ff7cf95f305072a6445abc9
Mike Zurzolo <mikezurzolo@gmail.com>
2020-02-25 22:48:35 -0500

    removed unused imports from accounts (cleanup checkpoint)

--- src/docker/django-docker/djangoapp/run.sh
+++ src/docker/django-docker/djangoapp/run.sh
@@ -4,5 +4,4 @@ pushd djangoapp
 . py3/bin/activate
 python3 /djangoapp/manage.py makemigrations
 python3 /djangoapp/manage.py migrate
-# python3 manage.py shell -c "from django.contrib.auth import get_user_model ; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
 python3 /djangoapp/manage.py runserver 0:8000
