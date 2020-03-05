52dcb18af48edfc467f8e4a110d17a7e099e6bad
Mike Zurzolo <mikezurzolo@gmail.com>
2020-03-05 10:01:49 -0500

    short-circuit post successful, changed the fields I was posting, and I think the event.preventDefault() may have contributed

--- src/docker/react-docker/frontend/src/App.js
+++ src/docker/react-docker/frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import axios from "axios";
 import Landing from './pages/Landing/Landing'
 import SignUp from './pages/Signup/Signup'
 import UserDemo from './containers/UsersDemo'
 import './App.css';
+
+//https://www.techiediaries.com/django-react-forms-csrf-axios/
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 
 export default class App extends React.Component {
@@ -13,7 +18,6 @@ export default class App extends React.Component {
         <SignUp />
         <UserDemo />
       </div>
-    );   
+    );
   }
 }
-
