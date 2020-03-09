89c51b74549038a64b4ffdc957bd7f8672628b48
Steven M. Douglass <sdouglass@uscold.com>
2020-03-08 20:32:39 -0400

    Successfully posting new users

--- src/docker/react-docker/frontend/src/containers/NewUser.jsx
+++ src/docker/react-docker/frontend/src/containers/NewUser.jsx
@@ -3,65 +3,59 @@ import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import "./newuser.css";
 import axios from "axios";
 
-
 export default function NewUser(props) {
     const [user, setUser] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
-
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
   }
 
   function handleSubmit(event) {
-    //event.preventDefault();
-    userAction();
+    event.preventDefault();
+    userPost();
   }
 
-   function userAction(){
+   function userPost(){
     axios.post("/api/users/", {
-        username: "AAHHH_test",
-        email: "ahtest@email.com"
+        username: user,
+        email: email
       });
-
 }
-
 
   return (
     <div className="NewUser">
       <form onSubmit={handleSubmit}>
-      <FormGroup controlId="user" bsSize="large">
-          <FormLabel>User Name</FormLabel>
-          <FormControl
-            autoFocus
-            type="user"
-            value={user}
-            onChange={e => setUser(e.target.value)}  />
-        </FormGroup>
-        <FormGroup controlId="email" bsSize="large">
-          <FormLabel>Email</FormLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <FormLabel>Password</FormLabel>
-          <FormControl
-            autoFocus
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </FormGroup>
-        <Button block bsSize="small" enabled={!validateForm()} type="submit" onclick={userAction()}>
-          Create Account
-
-               </Button>
+        <FormGroup controlId="user" >
+            <FormLabel>User Name</FormLabel>
+            <FormControl
+              autoFocus
+              type="user"
+              value={user}
+              onChange={e => setUser(e.target.value)}  />
+          </FormGroup>
+          <FormGroup controlId="email" >
+            <FormLabel>Email</FormLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" >
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              autoFocus
+              type="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </FormGroup>
+          <Button block type="submit" >
+            Create Account
+          </Button>
 
       </form>
     </div>
