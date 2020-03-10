59f6459a72f9370dd44077441d11b6d84c9b2de2
Mike Zurzolo <mikezurzolo@gmail.com>
2020-03-10 14:53:22 -0400

    registration looks like it's finally working? pre-cleanup checkpoint

--- src/docker/django-docker/djangoapp/usersrestful/urls.py
+++ src/docker/django-docker/djangoapp/usersrestful/urls.py
@@ -1,16 +1,25 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
+from rest_auth.registration.views import RegisterView, VerifyEmailView
+from rest_auth.views import LoginView, LogoutView, UserDetailsView, PasswordResetView, PasswordResetConfirmView, PasswordChangeView
 
 
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
 router.register(r"groups", views.GroupViewSet)
+router.register(r"register", views.Register, basename="register")
+router.register(r"register/account_confirm_email", views.ConfirmEmail, basename="account_confirm_email")
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("", include(router.urls)),
-    path("signup/", views.SignUp.as_view(), name="signup"),
+    path("rest-auth/", include("rest_auth.urls")),
     path("auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path('registration/verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
+    path('registration/', include('rest_auth.registration.urls')),
+    path('rest-auth/registration/account-email-verification-sent/', views.null_view, name='account_email_verification_sent'),
+    path('password-reset/confirm/<str:uidb64>)/<str:token>/', views.null_view, name='password_reset_confirm')
 ]
--- src/docker/django-docker/djangoapp/usersrestful/views.py
+++ src/docker/django-docker/djangoapp/usersrestful/views.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.models import Group
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.views import generic
 from .serializers import UserSerializer, GroupSerializer
 from .forms import CustomUserCreationForm
 from .models import RUser
+from rest_auth.registration import views
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -17,6 +21,7 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
+
 class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
@@ -24,6 +29,25 @@ class GroupViewSet(viewsets.ModelViewSet):
 
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+class Register(viewsets.ViewSetMixin, views.RegisterView):
+    pass
+
+
+class ConfirmEmail(viewsets.ViewSetMixin, views.VerifyEmailView):
+    pass
+
+
+
+@api_view()
+def null_view(request):
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view()
+def complete_view(request):
+    return Response("Email account is activated")
+
 
 
 class SignUp(generic.CreateView):
