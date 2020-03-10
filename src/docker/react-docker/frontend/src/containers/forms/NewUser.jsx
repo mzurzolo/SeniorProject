e4d31ea097c6a7701a16f1fe448b3eca25d01bfc
Steven M. Douglass <sdouglass@uscold.com>
2020-03-09 21:01:56 -0400

    Reworked NewUser.jsx

--- src/docker/react-docker/frontend/src/containers/forms/NewUser.jsx
+++ src/docker/react-docker/frontend/src/containers/forms/NewUser.jsx
@@ -1,59 +1,98 @@
 import React, { useState } from "react";
-import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import "./newuser.css";
 import axios from "axios";
 
 export default function NewUser(props) {
     const [user, setUser] = useState("");
     const [email, setEmail] = useState("");
+    const [email2, setEmail2] = useState("");
     const [password, setPassword] = useState("");
-
+    const [password2, setPassword2] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
     userPost();
   }
 
-   function userPost(){
+  function confirmPasswordMatch() {
+    if (password !== password2) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function confirmEmailMatch() {
+    if (email !== email2) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function userPost(){
+    // Validate the email and password
+    if (!confirmEmailMatch()) {
+      alert("Invalid request! \n Emails do not match");
+      return false;
+    } else if (!confirmPasswordMatch()) {
+      alert("Invalid request! \n Passwords do not match");
+      return false;
+    } 
+
+    // Call post to create a new user
     axios.post("/api/users/", {
         username: user,
         email: email
+      }).then(function (response) {
+        // If successful response (201)
+        if (response.status === 201) {
+          alert("Success! Your account has been created");
+        }
+      }).catch(function (error) {
+        alert("Invalid request! \n" + error);
       });
 }
 
   return (
     <div className="NewUser">
       <form onSubmit={handleSubmit}>
-        <FormGroup controlId="user" >
-            <FormLabel>User Name</FormLabel>
-            <FormControl
-              autoFocus
-              type="user"
-              value={user}
-              onChange={e => setUser(e.target.value)}  />
-          </FormGroup>
-          <FormGroup controlId="email" >
-            <FormLabel>Email</FormLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" >
-            <FormLabel>Password</FormLabel>
-            <FormControl
-              autoFocus
-              type="password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-          </FormGroup>
-          <Button block type="submit" >
-            Create Account
-          </Button>
-
+          <label>Username:</label>
+          <input
+            autoFocus
+            placeholder="Username"
+            required
+            type="text"
+            onChange={e => setUser(e.target.value)} />
+          <label>Email:</label>
+          <input
+            placeholder="Email"
+            type="email"
+            required
+            onChange={e => setEmail(e.target.value)} />
+          <label>Retype Email:</label>
+          <input
+            placeholder="Retype Email"
+            type="email"
+            required
+            onChange={e => setEmail2(e.target.value)} />
+          <label>Password:</label>
+          <input
+            placeholder="Password"
+            type="password"
+            minLength="8"
+            required
+            onChange={e => setPassword(e.target.value)}
+          />
+          <label>Retype Password:</label>
+          <input
+            placeholder="Retype Password"
+            type="password"
+            minLength="8"
+            required
+            onChange={e => setPassword2(e.target.value)}
+          />
+        <button type="submit">Create Account</button>
       </form>
     </div>
   );
