cb15480afa6a2ca497260f580f69664f06c39251
Mike Zurzolo <mikezurzolo@gmail.com>
2020-03-10 22:57:40 -0400

    chatroom-updates, password/admin stuff

--- src/docker/django-docker/djangoapp/djangoapp/asgi.py
+++ src/docker/django-docker/djangoapp/djangoapp/asgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.contrib.auth import get_user_model
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoapp.settings")
 
 application = get_asgi_application()
+
+# This still isn't a safe way to create an admin user...
+try:
+    User.objects.create_superuser("admin", "admin@example.com", "adminpass")
+
+except Exception as e:
+    print(e)
