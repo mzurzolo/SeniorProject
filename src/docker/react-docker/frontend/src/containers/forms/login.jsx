dd570e54044823b8dfd66a25821ff7827eefe3b7
Mike Zurzolo <mikezurzolo@gmail.com>
2020-03-11 14:57:37 -0400

    tweaked the compose file, tweaked some of the react stuff

--- src/docker/react-docker/frontend/src/containers/forms/login.jsx
+++ src/docker/react-docker/frontend/src/containers/forms/login.jsx
@@ -2,33 +2,29 @@ import React, { useState } from "react";
 import "./login.css";
 import {useHistory} from "react-router-dom";
 import axios from "axios";
-
 export default function Login(props) {
   const history = useHistory();
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
-
     function handleSubmit(event) {
       event.preventDefault();
       userLogin();
     }
-
     function userLogin(){
-
       // Call post to login user
       axios.post("/api/login/", {
           username: user,
           password: password
         }).then(function (response) {
-          // If successful response (201)
-          if (response.status === 201) {
+          // If successful response (200)
+          if (response.status === 200) {
             alert("Success! Your account has been logged in");
+            history.push("/UserDashboard")
           }
         }).catch(function (error) {
           alert("Invalid request! \n" + error);
         });
   }
-
     return (
       <div className="Login">
         <form onSubmit={handleSubmit}>
@@ -47,7 +43,12 @@ export default function Login(props) {
               required
               onChange={e => setPassword(e.target.value)}
             />
-          <button type="submit">Login</button>
+            <button block bsSize="small" type="submit" >
+            Login
+          </button>
+          <button block bsSize="small" type="submit" onClick={() => history.push("/Signup")}>
+          No account? --> Register
+          </button>
         </form>
       </div>
     );
