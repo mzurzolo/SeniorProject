1b140fcd78c857fe2efcf0bfc6c9eb6b48a249af
Mike Zurzolo <mikezurzolo@gmail.com>
2020-03-10 23:49:18 -0400

    login works from react now

--- src/docker/react-docker/frontend/src/containers/forms/login.jsx
+++ src/docker/react-docker/frontend/src/containers/forms/login.jsx
@@ -1,40 +1,54 @@
 import React, { useState } from "react";
 import "./login.css";
 import {useHistory} from "react-router-dom";
+import axios from "axios";
 
 export default function Login(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const history = useHistory();
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
 
-  function handleSubmit(event) {
-    event.preventDefault();
+    function handleSubmit(event) {
+      event.preventDefault();
+      userLogin();
+    }
+
+    function userLogin(){
+
+      // Call post to login user
+      axios.post("/api/login/", {
+          username: user,
+          password: password
+        }).then(function (response) {
+          // If successful response (201)
+          if (response.status === 201) {
+            alert("Success! Your account has been logged in");
+          }
+        }).catch(function (error) {
+          alert("Invalid request! \n" + error);
+        });
   }
 
-  return (
-    <div className="Login">
-      <form onSubmit={handleSubmit}>
-          <label for="email">Email</label>
-          <input
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-          <label>Password</label>
-          <input
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        
-        <button block bsSize="small" type="submit" >
-          Login
-        </button>
-        <button block bsSize="small" type="submit" onClick={() => history.push("/Signup")}>
-        No account? --> Register
-        </button>
-      </form>
-    </div>
-  );
-}
+    return (
+      <div className="Login">
+        <form onSubmit={handleSubmit}>
+            <label>Username:</label>
+            <input
+              autoFocus
+              placeholder="Username"
+              required
+              type="text"
+              onChange={e => setUser(e.target.value)} />
+            <label>Password:</label>
+            <input
+              placeholder="Password"
+              type="password"
+              minLength="8"
+              required
+              onChange={e => setPassword(e.target.value)}
+            />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
