46ae348bab0c1982fbb4d5f9fd97b59c5bc916b9
Mike Zurzolo <mikezurzolo@gmail.com>
2020-03-11 15:28:14 -0400

    bind-mount added to django container so we can do local development on windows machines -- trial 2

--- src/docker/django-docker/djangoapp/build.sh
+++ src/docker/django-docker/djangoapp/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-pushd djangoapp
-python3 -m venv py3
-. py3/bin/activate
+python3 -m venv env
+. /env/bin/activate
 python3 -m pip install -U pip
-python3 -m pip install -r requirements.txt
-popd
+python3 -m pip install -r /djangoapp/requirements.txt
