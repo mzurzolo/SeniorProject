04c8ec6d7587f7bb77b2aeeda59ef4fd3c2cd15c
Mike Zurzolo <mikezurzolo@gmail.com>
2020-03-25 07:57:17 -0400

    Fix: put the chat and websocket under /chat

--- src/docker/django-docker/djangoapp/chat/routing.py
+++ src/docker/django-docker/djangoapp/chat/routing.py
@@ -6,4 +6,4 @@ from . import views
 # websocket_urlpatterns = [
 #    url(r'^chat/(?P<label>[\w-]{,50}[^/]+)/$', consumers.ChatConsumer),
 # ]
-websocket_urlpatterns = [url(r"^(?P<label>[\w-]{,50})/$", consumers.ChatConsumer)]
+websocket_urlpatterns = [url(r"^chat/(?P<label>[\w-]{,50})/$", consumers.ChatConsumer)]
--- src/docker/django-docker/djangoapp/chat/urls.py
+++ src/docker/django-docker/djangoapp/chat/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r"^$", views.about, name="about"),
-    url(r"^new/$", views.new_room, name="new_room"),
-    url(r"^(?P<label>[\w-]{,50})/$", views.chat_room, name="chat_room"),
+    url(r"^chat/new/$", views.new_room, name="new_room"),
+    url(r"^chat/(?P<label>[\w-]{,50})/$", views.chat_room, name="chat_room"),
 ]
