5e58a4c7ede8eded2fdb48ed358585148ffa7c58
Steven M. Douglass <sdouglass@uscold.com>
2020-03-26 20:44:20 -0400

    Added Navbar component to Dashboard

---
+++ src/docker/react-docker/frontend/src/containers/media/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+export default function NavBar() {
+  return (
+    <div id='navbar'>
+      <p>Dashboard</p>
+      <p>User Profile</p>
+      <p>Sign Out</p>
+    </div>
+  );
+}
--- src/docker/react-docker/frontend/src/pages/UserDashboard/UserDashboard.js
+++ src/docker/react-docker/frontend/src/pages/UserDashboard/UserDashboard.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import NavBar from '../../containers/media/Navbar.jsx';
 
 export default class UserDashboard extends Component {
   render() {
     return (
       <div id='dashboard'>
+        <NavBar/>
         <h1>Dashboard</h1>
         <button block bsSize="small" type="button" onClick={() => this.props.history.push('/Game')}>Start Game
         </button>
