26bc0215369ef7db85c2966f3ffb0cfd26c43398
Steven M. Douglass <sdouglass@uscold.com>
2020-03-29 14:38:45 -0400

    Added styling and the leaderboard to the Dashboard

--- src/docker/react-docker/frontend/src/pages/UserDashboard/UserDashboard.js
+++ src/docker/react-docker/frontend/src/pages/UserDashboard/UserDashboard.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import NavBar from '../../containers/media/Navbar.jsx';
+import Leaderboard from '../../containers/media/Leaderboard';
+import './UserDashboard.css';
 
 export default class UserDashboard extends Component {
   render() {
     return (
       <div id='dashboard'>
         <NavBar/>
-        <h1>Dashboard</h1>
-        <button block bsSize="small" type="button" onClick={() => this.props.history.push('/Game')}>Start Game
-        </button>
+        <div id='startgame'>
+          <button block bsSize="small" type="button" onClick={() => 
+              this.props.history.push('/Game')}>Start Game</button>
+        </div>
+        <Leaderboard/>
       </div>
     );
   }
