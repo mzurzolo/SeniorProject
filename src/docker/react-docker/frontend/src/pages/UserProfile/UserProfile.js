609aaea271a233f42003f0f9a320c48f7ff6265e
JosephCBonsallV <47993109+JosephCBonsallV@users.noreply.github.com>
2020-04-10 17:05:46 -0400

    Update UserProfile.js
    Start of User Profile. Looks to check if you are currently logged in. and should display your username and email.

--- src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
+++ src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
 import NavBar from '../../containers/media/Navbar.jsx';
+import axios from 'axios';
 
 export default class UserProfile extends Component {
-  render() {
-    return (
-      <div id='userprofile'>
-        <NavBar/>
-        <h1>User Profile</h1>
+  async componentloggedin(){
+        try{
+        const user_profile = await axios.get('/d/acct/profile/').then(function(response) {
+        if (response.status === 200) {
+          return response.data;
+        }
+      }).catch(function(error) {
+        alert('Invalid request! \n' + error);
+      });
+    }
+      catch (e) {
+        console.log(e);
+      }
+    }
+
+      render() {
+        return (
+          <div id='userprofile'>
+            <NavBar/>
+          {this.state.user_profile.map(item =>(
+            <div key={item.id}>
+        <h1>User Profile </h1>
+        <h2>Username: {item.username}</h2>
+        <h2><span> Email: {item.email}</span></h2>
+        </div>
+          ))};
       </div>
     );
   }
