117f823d6e5b9211d13c0d639eecec2c65330f23
JosephCBonsallV <47993109+JosephCBonsallV@users.noreply.github.com>
2020-04-11 18:57:31 -0400

    Update UserProfile.js
    here we go

--- src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
+++ src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
@@ -5,23 +5,21 @@ import axios from 'axios';
 
 
 export default class UserProfile extends Component {
-  async UserInfo() {
-    axios.get('/d/acct/profile/').then((res) => {
-      if (res.status === 200) {
-        const curr_user = res.data;
-        curr_user.username = myusername;
-      }
-});
-  }
  render() {
     return (
-          <div id='userprofile'>
-            <NavBar/>
-        <h1>User Profile </h1>
-        <h2>Username: {myusername} </h2>
-        <h2><span> Email: </span></h2>
-        <h2><span> UUID: </span></h2>
-        </div>
+      axios.get('/d/acct/profile/').then((res) => {
+        if (res.status === 200) {
+          const curr_user = res.data;
+          curr_user.username = myusername;
+          curr_user.email = myemail;
+      <div id='userprofile'>
+          <NavBar/>
+      <h1>User Profile </h1>
+      <h2>Username: {myusername} </h2>
+      <h2><span> Email: {myemail}</span></h2>
+      <h2><span> UUID: </span></h2>
+      </div>
+      }})
     );
   }
 }
