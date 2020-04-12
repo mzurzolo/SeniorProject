7c4dcff9b5ad4f46daaf0189735221d0bda47a5d
JosephCBonsallV <47993109+JosephCBonsallV@users.noreply.github.com>
2020-04-11 21:56:52 -0400

    Update UserProfile.js

--- src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
+++ src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
@@ -9,18 +9,17 @@ export default class UserProfile extends Component {
   axios.get('/d/acct/profile/').then((res) => {
     if (res.status === 200) {
       const curr_user = res.data;
+    }
+});
     return (
       <div id='userprofile'>
           <NavBar/>
-      <div key={curr_user.id}>
       <h1>User Profile </h1>
       <h2>Username: {curr_user.username} </h2>
       <h2>Email: {curr_user.email} </h2>
       <h2>UUID: </h2>
       </div>
-      </div>  
-    );    
-    }
-  });
+      
+    );
   }
 }
