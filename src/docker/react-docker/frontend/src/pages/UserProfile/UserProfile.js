d951fb684cacb5aa51a6cc9f921883007450fc76
Vincent Paone <55220731+vpaone59@users.noreply.github.com>
2020-04-18 17:18:32 -0400

    Update UserProfile.js

--- src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
+++ src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
@@ -4,10 +4,23 @@ import axios from 'axios';
 
 
 
+
+
 export default class UserProfile extends Component {
   async componentDidMount() {
     axios.get('/d/acct/profile/').then((res) => {
-      console.log("this is working!")
+      <html>
+        <body>
+          <h1>My First Web Page</h1>
+          <p>My first paragraph.</p>
+          <script>
+            window.alert(5 + 6)
+           </script>
+
+        </body>
+      </html>
+
+
       if (res.status === 200) {
         console.log("this is working!")
         const curr_user = res.data;
