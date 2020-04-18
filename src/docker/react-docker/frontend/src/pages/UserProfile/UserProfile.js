bc4b613379944cbad3e24729b95a5c3d38f52643
Vincent Paone <55220731+vpaone59@users.noreply.github.com>
2020-04-18 17:05:10 -0400

    Update UserProfile.js

--- src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
+++ src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 
 export default class UserProfile extends Component {
-  render() {
+  async componentDidMount() {
     axios.get('/d/acct/profile/').then((res) => {
       if (res.status === 200) {
         console.log("this is working!")
@@ -13,16 +13,18 @@ export default class UserProfile extends Component {
         const curr_username = curr_user.username;
         const curr_email = curr_user.email;
       }
-      return (
-        <div id='userprofile'>
-          <NavBar />
-          <h1>User Profile </h1>
-          <h2>Username: {curr_username}</h2>
-          <h2>Email: </h2>
-          <h2>UUID: </h2>
-        </div>
-      );
     })
+  }
+  render() {
+    return (
+      <div id='userprofile'>
+        <NavBar />
+        <h1>User Profile </h1>
+        <h2>Username: {curr_username}</h2>
+        <h2>Email: </h2>
+        <h2>UUID: </h2>
+      </div>
+    );
   }
 }
 
