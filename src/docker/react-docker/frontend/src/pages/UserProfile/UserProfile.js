e2bf99d41361bdbed4298ad02b5fe20629766684
JosephCBonsallV <47993109+JosephCBonsallV@users.noreply.github.com>
2020-04-18 17:48:25 -0400

    Update UserProfile.js

--- src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
+++ src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import NavBar from '../../containers/media/Navbar.jsx';
-import axios from 'axios';
-
+//import axios from 'axios';
+// axios.get('/d/acct/profile/').then((res) => {
+      //if (res.status === 200) {
+         // const curr_user = res.data;
+         //})
+         
 export default class UserProfile extends Component {
   async componentDidMount() {
-    axios.get('/d/acct/profile/').then((res) => {
-      if (res.status === 200) {
-          const curr_user = res.data;
-          window.alert(5 + 6);
-      }
-    })
+  try {
+
+    const res = await fetch('/acct/profile/')
+    const profile = await res.json();
+    this.setState({
+      profile
+    });
+    
+    window.alert(5 + 6);
+    
+  } catch (e) {
+    console.log(e);
+  }
   }
   render() {
     return (
       <div id='userprofile'>
         <NavBar />
+        {this.profile.map(item => (
+          <div key={item.id}>
         <h1>User Profile </h1>
-        <h2>Username:</h2>
-        <h2>Email: </h2>
+        <h2>Username:{item.username}</h2>
+        <h2>Email: {item.email}</h2>
         <h2>UUID: </h2>
       </div>
+        ))}
+    </div>
     );
   }
 }
