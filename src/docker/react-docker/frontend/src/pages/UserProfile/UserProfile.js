0d466688daedb4901dff1bc91c7d87df6d925156
Vincent Paone <55220731+vpaone59@users.noreply.github.com>
2020-04-18 17:01:19 -0400

    Testing console output

--- src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
+++ src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
@@ -1,25 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import NavBar from '../../containers/media/Navbar.jsx';
 import axios from 'axios';
 
 
 
 export default class UserProfile extends Component {
-  render() {  
+  render() {
     axios.get('/d/acct/profile/').then((res) => {
-    if (res.status === 200) {
-    const curr_user = res.data;
-    const curr_username = curr_user.username;
-    const curr_email =curr_user.email;
-    }
-    return (
-      <div id='userprofile'>
-          <NavBar/>
-      <h1>User Profile </h1>
-      <h2>Username: {curr_username}</h2>
-      <h2>Email: </h2>
-      <h2>UUID: </h2>
-      </div>
-    );
-  })}}
+      if (res.status === 200) {
+        console.log("this is working!")
+        const curr_user = res.data;
+        const curr_username = curr_user.username;
+        const curr_email = curr_user.email;
+      }
+      return (
+        <div id='userprofile'>
+          <NavBar />
+          <h1>User Profile </h1>
+          <h2>Username: {curr_username}</h2>
+          <h2>Email: </h2>
+          <h2>UUID: </h2>
+        </div>
+      );
+    })
+  }
+}
 
