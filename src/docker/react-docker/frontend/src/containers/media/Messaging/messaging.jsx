b4b82984e858e14c877a4cc789a8a9fe544e21db
Mike Zurzolo <mikezurzolo@gmail.com>
2020-04-24 21:16:05 -0400

    removed Messdaging, deleted some css stuff

---
+++ src/docker/react-docker/frontend/src/containers/media/Messaging/messaging.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import InitChat from './'
+import Chat from './'
+import WebSocketInstance from './WebSocket'
+
+export default class M extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            loggedIn: false
+        };
+    }
+
+    handleLoginSubmit = (username) => {
+        this.setState({ loggedIn: true, username: username });
+        WebSocketInstance.connect();
+    }
+
+    render() {
+        const {
+            loggedIn,
+            username
+        } = this.state;
+
+        return (
+            <div className="App">
+                {
+                    loggedIn ?
+                        <Chat
+                            currentUser={username}
+                        />
+                        :
+                        <InitChat
+                            onSubmit={this.handleLoginSubmit}
+                            usernameChangeHandler={this.usernameChangeHandler}
+                        />
+                }
+            </div>
+        );
+    }
+}
