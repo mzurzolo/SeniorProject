b4578292ce4879a59e545bc088c655c5de7c58b8
Vincent Paone <55220731+vpaone59@users.noreply.github.com>
2020-04-24 20:33:32 -0400

    new Chat (!)

--- src/docker/django-docker/djangoapp/chat/consumers.py
+++ src/docker/django-docker/djangoapp/chat/consumers.py
@@ -1,7 +1,11 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from . import models
+from .models import Message
 from channels.db import database_sync_to_async
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 # ToDo Item #2 (hopeful for code reuse from src/docker/django-docker/djangoapp/{chat,static}) The game and the chat are not integrated.
 # ToDo Item #3 (hopeful for code reuse from src/docker/django-docker/djangoapp/{chat,static}) The chat and the backend are not integrated.
 # ToDo Item #4 The chat does not use usernames; it allows you to define your own handle.
@@ -9,6 +13,58 @@ from channels.db import database_sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     name = "ChatConsumer"
+
+    def init_chat(self, data):
+        username = data['username']
+        user = User.objects.get(username=username)
+        content = {
+            'command': 'init_chat'
+        }
+        if not user:
+            content['error'] = 'Unable to get or create User with username: ' + username
+            self.send_message(content)
+        content['success'] = 'Chatting in with success with username: ' + username
+        self.send_message(content)
+
+            def fetch_messages(self, data):
+        messages = Message.last_50_messages()
+        content = {
+            'command': 'messages',
+            'messages': self.messages_to_json(messages)
+        }
+        self.send_message(content)
+
+    def new_message(self, data):
+        author = data['from']
+        text = data['text']
+        author_user, created = User.objects.get_or_create(username=author)
+        message = Message.objects.create(author=author_user, content=text)
+        content = {
+            'command': 'new_message',
+            'message': self.message_to_json(message)
+        }
+        self.send_chat_message(content)
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+        return result
+
+    def message_to_json(self, message):
+        return {
+            'id': str(message.id),
+            'author': message.author.username,
+            'content': message.content,
+            'created_at': str(message.created_at)
+        }
+
+    commands = {
+        'init_chat': init_chat,
+        'fetch_messages': fetch_messages,
+        'new_message': new_message
+    }
+
 
     async def websocket_connect(self, event):
         self.room_name = self.scope["url_route"]["kwargs"]["label"]
---
+++ src/docker/react-docker/frontend/src/containers/media/Messaging/config.js
@@ -0,0 +1,5 @@
+const API_PATH = '/d/chat';
+
+export default {
+    API_PATH
+};
