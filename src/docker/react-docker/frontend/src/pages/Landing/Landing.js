f562f0e55b82be3870e86779fb287c9f68dc8f55
JosephCBonsallV <47993109+JosephCBonsallV@users.noreply.github.com>
2020-04-28 14:00:08 -0400

    Update Landing.js

--- src/docker/react-docker/frontend/src/pages/Landing/Landing.js
+++ src/docker/react-docker/frontend/src/pages/Landing/Landing.js
@@ -4,14 +4,12 @@ import Login from '../../containers/forms/login';
 import GameDescription from '../../containers/media/GameDescription';
 import GamePicture from '../../containers/media/GamePicture';
 import Leaderboard from '../../containers/media/Leaderboard';
-import NavBar from '../../containers/media/Navbar.jsx';
 import axios from 'axios';
 
 export default class Landing extends Component {
   render() {
     return (
       <div id='landing'>
-        <NavBar/>
         <GameDescription />
         <Login />
         <GamePicture />
