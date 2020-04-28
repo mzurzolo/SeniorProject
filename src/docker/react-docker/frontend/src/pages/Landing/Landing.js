dc0b3aa48cb283db14427de401aea66fea4a16a0
JosephCBonsallV <47993109+JosephCBonsallV@users.noreply.github.com>
2020-04-28 13:49:56 -0400

    Updates to Chat.js and Landing.js
    Updates

--- src/docker/react-docker/frontend/src/pages/Landing/Landing.js
+++ src/docker/react-docker/frontend/src/pages/Landing/Landing.js
@@ -4,12 +4,7 @@ import Login from '../../containers/forms/login';
 import GameDescription from '../../containers/media/GameDescription';
 import GamePicture from '../../containers/media/GamePicture';
 import Leaderboard from '../../containers/media/Leaderboard';
-<<<<<<< Updated upstream
-
-
-=======
 import axios from 'axios';
->>>>>>> Stashed changes
 
 export default class Landing extends Component {
   render() {
