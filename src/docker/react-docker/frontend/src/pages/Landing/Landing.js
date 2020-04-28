4c06c8730861c704df288a05244b6573621ec87f
JosephCBonsallV <47993109+JosephCBonsallV@users.noreply.github.com>
2020-04-27 22:38:24 -0400

    Update Landing.js
    Updating Landing to only be accessed if logged in.

--- src/docker/react-docker/frontend/src/pages/Landing/Landing.js
+++ src/docker/react-docker/frontend/src/pages/Landing/Landing.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
 import './Landing.css';
-
+import axios from 'axios';
 import Login from '../../containers/forms/login';
 import GameDescription from '../../containers/media/GameDescription';
 import GamePicture from '../../containers/media/GamePicture';
 import Leaderboard from '../../containers/media/Leaderboard';
+import {useHistory} from 'react-router-dom';
 
 
 export default class Landing extends Component {
+  async componentDidMount() {
+    const history = useHistory();
+    try{
+  axios.get('/d/acct/profile/').then((res) => {
+    if (res.status === 200) {
+      history.push('/Dashboard');
+    }
+    }).catch(function(error) {
+        alert('Invalid request! \n' + error);
+      });
+    }
+  }
   render() {
     return (
       <div id='landing'>
