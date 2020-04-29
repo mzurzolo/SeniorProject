7b6884c6c8d7eeb49d1b3aa2f28972f957b5d0f2
Mike Zurzolo <mikezurzolo@gmail.com>
2020-04-28 23:02:07 -0400

    oops, referenced game before assignment

--- src/docker/django-docker/djangoapp/game/views.py
+++ src/docker/django-docker/djangoapp/game/views.py
@@ -51,10 +51,11 @@ class GameViewSet(viewsets.ModelViewSet):
                     models.Game.objects.filter(id=game.id).update(player_2=request.user)
                     game = models.Game.objects.get(id=game.id)
                     game.gamestate['player2'] = request.user.username
+                    print(game.gamestate['player1'], file=sys.stderr)
                     game.save()
 
         # serializer = self.get_serializer(game, many=False)
-        print(game.gamestate['player1'], file=sys.stderr)
+
         with transaction.atomic():
             games = list(
                 models.Game.objects.all()
