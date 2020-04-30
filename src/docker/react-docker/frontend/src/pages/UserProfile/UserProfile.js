223634431b9cc4680e69d5a84a8396af2f156733
Steve940 <smd940@gmail.com>
2020-04-30 11:03:00 -0400

    Adjusting UI

--- src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
+++ src/docker/react-docker/frontend/src/pages/UserProfile/UserProfile.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import NavBar from '../../containers/media/Navbar.jsx';
 import axios from 'axios';
+import './UserProfile.css';
 
 
 export default class UserProfile extends Component {
@@ -25,13 +26,19 @@ export default class UserProfile extends Component {
     return (
       <div id='userprofile'>
         <NavBar/>
-        <h1>User Profile </h1>
-        <h2>Username: {this.state.curr_user.username}</h2>
-        <h2>Email: {this.state.curr_user.email}</h2>
-        <h2>First Name: {this.state.curr_user.first_name}</h2>
-        <h2>Last Name: {this.state.curr_user.last_name}</h2>
-        <button block bsSize="small" type="button" onClick={() =>
-            this.props.history.push('/UserEdit')}>Edit Profile</button>
+        <div className='container'>
+          <div id='userprofileform'>
+            <div>
+              <h1>User Profile </h1>
+            </div>
+            <h2>Username: {this.state.curr_user.username}</h2>
+            <h2>Email: {this.state.curr_user.email}</h2>
+            <h2>First Name: {this.state.curr_user.first_name}</h2>
+            <h2>Last Name: {this.state.curr_user.last_name}</h2>
+            <button block bsSize="small" type="button" onClick={() =>
+              this.props.history.push('/UserEdit')}>Edit Profile</button> 
+          </div>
+        </div>
       </div>
     );
   }
